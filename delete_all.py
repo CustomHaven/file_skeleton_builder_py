feb59c057b5d7a46d116bfa75141b255fe1fa8d9
CustomHaven <mmuseaden@gmail.com>
2025-04-02 00:09:52 +0100

    robust allows user to exit gracefully

--- delete_all.py
+++ delete_all.py
@@ -1,9 +1,14 @@
 import os
 
+def check_dir(directory):
+    return os.path.isdir(directory)
+
 def delete_file(root, file):
     os.remove(os.path.join(root, file))
 
 def delete_directory_and_contents(directory):
+    if check_dir(directory) == False:
+        return
     for root, dirs, files in os.walk(directory, topdown=False):  # Walk through directories bottom-up
         for file in files:
             delete_file(root, file)
@@ -19,7 +24,7 @@ excluded_extensions = [".json", ".git", ".gitignore"]
 items_to_delete = [item for item in all_dirs if item not in excluded_files and not any(item.endswith(ext) for ext in excluded_extensions)]
 
 for item in items_to_delete:
-    if os.path.isdir(item):
+    if check_dir(item):
         delete_directory_and_contents(item)
     else:
         delete_file(os.getcwd(), item)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import delete_all
 import create_all
 
 if __name__ == "__main__":
     while True:
         try:
-            user_input = input("Enter name of the parent directory:\n1. Backend or 2. Frontend: ")
+            user_input = input("Enter name of the parent directory:\n1. Backend, 2. Frontend, or 0. End: ")
 
-            if user_input not in ["1", "2", "backend", "frontend"]:
-                raise ValueError("Invalid input! Please choose '1', as 'backend' or '2', 'frontend'.")
+            if user_input.lower() in ["0", "end"]:
+                delete_all.delete_directory_and_contents("__pycache__")
+                break
+            if user_input.lower() not in ["1", "2", "backend", "frontend"]:
+                raise ValueError("Invalid input! Please choose '1', as 'backend' or '2', as 'frontend'. Or you can enter '0' or 'end' to exit.")
 
             if user_input == "1" or user_input == "2":
                 create_all.src = ["backend", "frontend"][int(user_input) -1]
